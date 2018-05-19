70c6f9468954641f78a40f1a7b1e19fce2a24af6
AyanaZaire <ayanazaire@gmail.com>
2018-05-19 01:49:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -13,3 +13,8 @@ function updatePlaylist(playlist, artist, song){
 }
 
 //Create a function removeFromPlaylist that accepts two arguments (the playlist object and the artist name). The body of the function should delete the key-value pair from the playlist and return the updated playlist.
+
+function removeFromPlaylist(playlist, artist){
+  delete 
+  return playlist
+}

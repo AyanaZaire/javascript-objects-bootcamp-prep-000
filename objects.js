e64472d76376c80063d2776f0606b1aaf9fc826b
AyanaZaire <ayanazaire@gmail.com>
2018-05-19 01:29:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 //Assign an object to the variable playlist and initialize the object with a key-value pair — the keys will be artist names and the values will be song titles. (What limitation does this impose on our playlist?)
 
 var playlist = {
-  Frank ocean: "Nights"
+  Beyonce: "Nights",
   Solange: "Cranes in the Sky"
-};
+}

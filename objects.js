317232f179401488b06e5f08c098895e22b3206c
AyanaZaire <ayanazaire@gmail.com>
2018-05-19 01:32:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,11 @@
+//
+
 var playlist = {
   Beyonce: "Nights",
   Solange: "Cranes in the Sky"
 }
 
-//Create a function removeFromPlaylist that accepts two arguments (the playlist object and the artist name). The body of the function should delete the key-value pair from the playlist and return the updated playlist.
+//Create a function updatePlaylist that accepts three parameters: the playlist (an object), an artist name (a string), and a song title. The body of the function should add the song and artist as a key-value pair to the playlist object. The function should return the whole playlist.
 
 function updatePlaylist(){
   

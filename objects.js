b958c677fd03950a33b14acb35d45a46686e0383
AyanaZaire <ayanazaire@gmail.com>
2018-05-19 01:29:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,7 @@ var playlist = {
   Beyonce: "Nights",
   Solange: "Cranes in the Sky"
 }
+
+//Create a function updatePlaylist that accepts three parameters: the playlist (an object), an artist name (a string), and a song title. The body of the function should add the song and artist as a key-value pair to the playlist object. The function should return the whole playlist.
+
+//
